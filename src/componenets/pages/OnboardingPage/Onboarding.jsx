d8d4ecb4733f196c67c0392b4830242ee3677bce
mohamed altumi <moaltumi@gmail.com>
2024-01-24 16:30:00 -0800

    footer deleted

--- src/componenets/pages/OnboardingPage/Onboarding.jsx
+++ src/componenets/pages/OnboardingPage/Onboarding.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
+import React from "react";
 import "./Onboarding.scss";
 import logo from "../../../assets/images/Creativehub.png";
-import {Link} from "react-router-dom";
-import footer from "../../Footer/Footer.jsx";
-
+import { Link } from "react-router-dom";
 
 function Onboarding() {
-    return (
-      <> 
-        <Link to = "/">
+  return (
+    <>
+      <Link to="/">
         <img className="logo" src={logo} alt="logo" />
-        </Link>
+      </Link>
 
-        <div className= "onboarding">
-            <h1 className = "onboarding__title">Welcome to Creativehub</h1>
-            <h2 className = "onboarding__subtitle">Connect with Influencers to Skyrocket Your Product's Reach</h2>
-            <button className = "onboarding__button">Get Started With Instagram</button>
-            <p className = "onboarding__text">Already have an account? <Link className = "onboarding__text__signin">Signin</Link></p>
-
-
-        </div>
-        </>
-    );
-};
+      <div className="onboarding">
+        <h1 className="onboarding__title">Welcome to Creativehub</h1>
+        <h2 className="onboarding__subtitle">
+          Connect with Influencers to Skyrocket Your Product's Reach
+        </h2>
+        <button className="onboarding__button">
+          Get Started With Instagram
+        </button>
+        <p className="onboarding__text">
+          Already have an account?{" "}
+          <Link className="onboarding__text__signin">Signin</Link>
+        </p>
+      </div>
+    </>
+  );
+}
 export default Onboarding;
