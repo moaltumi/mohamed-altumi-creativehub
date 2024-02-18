4151cbf8a52fbcc9964452f0b27d3d47bba434b1
mohamed altumi <moaltumi@gmail.com>
2024-02-17 16:53:52 -0800

    feeds and discover page responsive

--- src/componenets/pages/Feeds/Feeds.jsx
+++ src/componenets/pages/Feeds/Feeds.jsx
@@ -36,93 +36,91 @@ const Feeds = () => {
   useEffect(() => {}, []);
 
   return (
-    <>
-      <div className="single-feed">
-        <div className="single-feed__header">
-          <Link to="/">
-            <img className="single-feed__header__logo" src={logo} alt="logo" />
-          </Link>
+    <div className="single-feed">
+      <div className="single-feed__header">
+        <Link to="/">
+          <img className="single-feed__header__logo" src={logo} alt="logo" />
+        </Link>
 
-          <div className="single-feed__header__discover">
-            <Link to="/discoverbrands">
-              <img
-                className="single-feed__header__discover__icon"
-                src={discoverbrand}
-                alt="Discover Brands"
-              />
-            </Link>
-          </div>
-        </div>
-
-        <div className="profile-card">
-          <div className="profile-card__rowone">
-            <Link
-              to="https://www.instagram.com/altumitravels/"
-              className="profile-card__rowone__info"
-            >
-              <img
-                className="profile-card__rowone__info__logo"
-                alt="iglogo"
-                src={instagramlogo}
-              />
-              <p className="profile-card__rowone__info__text">altumitravels</p>
-            </Link>
-
+        <div className="single-feed__header__discover">
+          <Link to="/discoverbrands">
             <img
-              className="profile-card__rowone__image"
-              src={profileimage}
-              alt="Profile"
+              className="single-feed__header__discover__icon"
+              src={discoverbrand}
+              alt="Discover Brands"
             />
-
-            <div className="profile-card__rowone__stats">
-              <p className="profile-card__rowone__stat-value">34</p>
-              <p className="profile-card__rowone__stat-label">Posts</p>
-            </div>
-            <div className="profile-card__rowone__stats">
-              <p className="profile-card__rowone__stat-value">691</p>
-              <p className="profile-card__rowone__stat-label">followers</p>
-            </div>
-
-            <div className="profile-card__rowone__stats">
-              <p className="profile-card__rowone__stat-value">1022</p>
-              <p className="profile-card__rowone__stat-label">following</p>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className="profile-card__rowthree"
-          style={{ display: apiCalled ? "none" : "flex" }}
-        >
-          <h1 className="profile-card__rowthree__title">Set up your profile</h1>
-          <p className="profile-card__rowthree__subtitle">
-            {" "}
-            Get started on your journey to meaningful connections and impactful
-            collaborations now!
-          </p>
-          <div className="profile-card__rowthree__buttons">
-            <button
-              className="profile-card__rowthree__buttons__upload-ig"
-              onClick={handleUploadFromInstagram}
-            >
-              Import from Instagram
-            </button>
-          </div>
-        </div>
-
-        <div className="profile-card__galery">
-          {imageData.map((image, index) => (
-            <div className="card" key={index}>
-              <img
-                className="profile-card__galery__image"
-                src={image.media_url}
-                alt="instagram post"
-              />
-            </div>
-          ))}
+          </Link>
         </div>
       </div>
-    </>
+
+      <div className="profile-card">
+        <div className="profile-card__rowone">
+          <Link
+            to="https://www.instagram.com/altumitravels/"
+            className="profile-card__rowone__info"
+          >
+            <img
+              className="profile-card__rowone__info__logo"
+              alt="iglogo"
+              src={instagramlogo}
+            />
+            <p className="profile-card__rowone__info__text">altumitravels</p>
+          </Link>
+
+          <img
+            className="profile-card__rowone__image"
+            src={profileimage}
+            alt="Profile"
+          />
+
+          <div className="profile-card__rowone__stats">
+            <p className="profile-card__rowone__stat-value">34</p>
+            <p className="profile-card__rowone__stat-label">Posts</p>
+          </div>
+          <div className="profile-card__rowone__stats">
+            <p className="profile-card__rowone__stat-value">691</p>
+            <p className="profile-card__rowone__stat-label">followers</p>
+          </div>
+
+          <div className="profile-card__rowone__stats">
+            <p className="profile-card__rowone__stat-value">1022</p>
+            <p className="profile-card__rowone__stat-label">following</p>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="profile-card__rowthree"
+        style={{ display: apiCalled ? "none" : "flex" }}
+      >
+        <h1 className="profile-card__rowthree__title">Set up your profile</h1>
+        <p className="profile-card__rowthree__subtitle">
+          {" "}
+          Get started on your journey to meaningful connections and impactful
+          collaborations now!
+        </p>
+        <div className="profile-card__rowthree__buttons">
+          <button
+            className="profile-card__rowthree__buttons__upload-ig"
+            onClick={handleUploadFromInstagram}
+          >
+            Import from Instagram
+          </button>
+        </div>
+      </div>
+
+      <div className="profile-card__galery">
+        {imageData.map((image, index) => (
+          <div className="card" key={index}>
+            <img
+              className="profile-card__galery__image"
+              src={image.media_url}
+              alt="instagram post"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
